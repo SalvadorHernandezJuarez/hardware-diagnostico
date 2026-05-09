65b0eb1813559ad7c52da1072debc6281bff3d0f
salvador2073830 <salvador.hernandezj@uanl.edu.mx>
2026-05-09 13:50:59 -0600

    Se creo main.py y requirements.txt

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+DIAGNOSTICO - Herramienta de Diagnóstico de Hardware
+Punto de entrada principal
+"""
+
+from ui.menu import Menu
+
+
+if __name__ == "__main__":
+    app = Menu()
+    app.run()
